b819ddcc74b2a59d3d9d56293eb3285d9df29f21
Agil Adi Saputro <agiladisaputro@gmail.com>
2025-06-10 02:56:27 +0700

    Modified env example and migration

--- scripts/migrate.sql
+++ scripts/migrate.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS app_conofig (
+CREATE TABLE IF NOT EXISTS app_config (
     key VARCHAR(100) PRIMARY KEY,
     value TEXT NOT NULL,
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 )
 
 -- seed default value
-INSERT INTO app_conofig (key, value)
+INSERT INTO app_config (key, value)
 VALUES ('max_load', '5')
 ON CONFLICT (key) DO NOTHING;

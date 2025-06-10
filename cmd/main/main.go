739d9aa5e955f3028abe11626031bc83266745f0
Agil Adi Saputro <agiladisaputro@gmail.com>
2025-06-10 13:20:46 +0700

    Add logic to get and update max_load

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -8,11 +8,14 @@ import (
 
 	"github.com/agiladis/custom-agent-allocation/internal/config"
 	v1 "github.com/agiladis/custom-agent-allocation/internal/handler/v1"
+	"github.com/agiladis/custom-agent-allocation/internal/repository"
 	"github.com/agiladis/custom-agent-allocation/internal/service"
 	"github.com/gofiber/fiber/v2"
 	"github.com/redis/go-redis/v9"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -26,16 +29,16 @@ func main() {
 	}
 
 	// connect to postgres
-	// dsn := "host=" + cfg.DBHost +
-	// 	" user=" + cfg.DBUsername +
-	// 	" password=" + cfg.DBPassword +
-	// 	" dbname=" + cfg.DBName +
-	// 	" port=" + cfg.DBPort +
-	// 	" sslmode=" + cfg.DBSSLMode
-	// db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	// if err != nil {
-	// 	log.Fatal().Err(err).Msg("failed to connect to Postgres")
-	// }
+	dsn := "host=" + cfg.DBHost +
+		" user=" + cfg.DBUsername +
+		" password=" + cfg.DBPassword +
+		" dbname=" + cfg.DBName +
+		" port=" + cfg.DBPort +
+		" sslmode=" + cfg.DBSSLMode
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to connect to Postgres")
+	}
 
 	// connect to redis
 	rdb := redis.NewClient(&redis.Options{
@@ -46,25 +49,29 @@ func main() {
 		log.Fatal().Err(err).Msg("failed to connect to Redis")
 	}
 
-	// init fiber
-	app := fiber.New()
-
 	// init repo, service, handler
 	ctx := context.Background()
-	publisher, err := service.NewPublisher(ctx, rdb, cfg)
+	cfgRepo := repository.NewConfigRepository(db, rdb)
+	cfgSvc := service.NewConfigService(cfgRepo)
+	cfgHandler := v1.NewConfigHandler(cfgSvc)
+
+	pub, err := service.NewPublisher(ctx, rdb, cfg)
 	if err != nil {
 		log.Fatal().Err(err).Msg("cannot create publisher")
 	}
-	webhookHandler := v1.NewWebhookHandler(publisher)
+	webhookHandler := v1.NewWebhookHandler(pub)
 
-	// Routes
+	// Fiber and Routes
+	app := fiber.New()
 	v1 := app.Group("/api/v1")
+	v1.Get("/config/max-load", cfgHandler.GetMaxLoad)
+	v1.Patch("/config/max-load", cfgHandler.UpdateMaxLoad)
 	v1.Post("/webhook/custom-agent-allocation", webhookHandler.Receive)
 
-	// graceful shutdown
+	// start server with graceful shutdown
 	go func() {
 		if err := app.Listen(":" + cfg.Port); err != nil {
-			log.Fatal().Err(err).Msg("fiber shutdown failed")
+			log.Fatal().Err(err).Msg("Server error")
 		}
 	}()
 
